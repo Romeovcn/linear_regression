f228ce4e63a2e910db211a968fec3f6fd3470f14
romeo <vincent-romeo@hotmail.fr>
2024-11-04 18:04:49 +0100

    matrix working

--- normalizer.py
+++ normalizer.py
@@ -1,36 +1,12 @@
 import copy
 
-def normalize_dataset(dataset_array):
-	normalized_array = copy.deepcopy(dataset_array)
-	array = [obj['km'] for obj in dataset_array]
-	mean = get_mean(array)
-	sigma = get_sigma(array)
-	for element in normalized_array:
-		# print(element)
-		element['km'] = (element['km'] - mean) / sigma
-	return normalized_array
+def unnormalize_thetas(X, thetas):
+	feat_mean = get_mean(X)
+	feat_sigma = get_sigma(X)
 
-def unnormalize_thetas(thetaA, thetaB, dataset_array):
-	array = [obj['km'] for obj in dataset_array]
-	target_array = [obj['price'] for obj in dataset_array]
-
-	feat_mean = get_mean(array)
-	feat_sigma = get_sigma(array)
-
-	thetaA = thetaA / feat_sigma
-	thetaB = thetaB - (thetaA * feat_mean)
-	return (thetaA, thetaB)
-
-# def normalize_value(value, dataset_array):
-# 	mean = get_mean(dataset_array)
-# 	sigma = get_sigma(dataset_array)
-# 	return (value - mean) / sigma
-
-# def unnormalize_value(value, dataset_array):
-# 	array = [obj['km'] for obj in dataset_array]
-# 	mean = get_mean(dataset_array)
-# 	sigma = get_sigma(dataset_array)
-# 	return value * sigma + mean
+	thetaA = thetas[1] / feat_sigma
+	thetaB = thetas[0] - (thetaA * feat_mean)
+	return (thetaB, thetaA)
 
 def get_mean(array):
 	mean = 0
